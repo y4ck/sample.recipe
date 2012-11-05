74649b724c28ca265d3a5b3f819bc9421345b0f6
y4ck <y4ck@vk1067.megiteam.com.pl>
2012-11-05 17:09:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRecipe::Application.config.secret_token = '3a0a4d95baa1d8b75010e2ee24839bea294a1e2298fc7afc045106acddaec313d49e9ca9be1cde638ac31b612f55c6a39ca5c5a800d1dc76ebffc27b62135c9d'
